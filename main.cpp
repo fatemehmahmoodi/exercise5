11bb81ef4e1e0dd13479c7104d5310c19ff9b50e
fatemehmahmoodi <116880900+fatemehmahmoodi@users.noreply.github.com>
2022-11-03 21:03:51 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void drawCircle();
+int main()
+{
+    drawCircle();
+    return 0;
+}
+    void drawCircle()
+    {
+    cout<<"\n * * * *";
+    cout<<"\n *";
+    cout<<"\n * * *";
+    cout<<"\n *";
+    cout<<"\n *";
+    cout<<"\n *";
+
+    }
+
